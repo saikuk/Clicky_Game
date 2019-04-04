a7189edd235da2d9440f0dee9721b0e3caece2bd
Sai Kuk <kukcheong@gmail.com>
2019-04-03 20:58:15 -0400

    Updates

---
+++ precache-manifest.976c29cbdb408be422df72e3b9408ce5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef6ad64b0df3315f3b6a",
+    "url": "/clicky_game/static/js/runtime~main.33627756.js"
+  },
+  {
+    "revision": "ca2d90a02834c744e792",
+    "url": "/clicky_game/static/js/main.9ca50db8.chunk.js"
+  },
+  {
+    "revision": "a686e41154fd92225c54",
+    "url": "/clicky_game/static/js/2.5bfccd3c.chunk.js"
+  },
+  {
+    "revision": "ca2d90a02834c744e792",
+    "url": "/clicky_game/static/css/main.d52aac33.chunk.css"
+  },
+  {
+    "revision": "ee80a0a48074022a086f753ebcc6fa26",
+    "url": "/clicky_game/index.html"
+  }
+];
